6ac90e62d7915b61b615c909c23e449395fc4e47
Dhanush-max <DhanushKumarM003@gmail.com>
2023-05-10 06:47:03 +0000

    renamed app into main

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ app = Flask(__name__)
 food = [
   {
     'id':1,
-    'name':'Dosa',
-    'Rating':'9/10'
+    'name':'eth',
+    'price':'$4000'
   },
   {
     'id':2,
-    'name':'idly',
-    'Rating':'6/10'
+    'name':'polygon',
+    'price':'$20'
   },
   {
     'id':3,
-    'name':'Rice',
-    'Rating':'7/10'
+    'name':'shiba',
+    'price':'$0.0123'
   },
   {
     'id':4,
-    'name':'Biriyani',
-    'Rating':'9.5/10'
+    'name':'bitcoin',
+    'price':'$21,200'
   }
 ]
 
